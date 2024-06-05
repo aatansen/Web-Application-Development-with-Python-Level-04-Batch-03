e68ab7853d6aaa7985a25e4f30367ff45640b6c1
Md. Alahi Almin Tansen <aatansen@gmail.com>
2024-06-05 23:02:50 +0600

    Job Portal Project Recap 04 Signup fields validated

--- Day-54-Job Portal Project Recap 4/jobProject/jobProject/views.py
+++ Day-54-Job Portal Project Recap 4/jobProject/jobProject/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import login,logout,authenticate
+from django.contrib import messages
 from jobApp.models import *
+import re
+
+message_box={
+    "username_warning":"Username not valid",
+    "password_warning":"Password weak",
+    "city_warning":"City name not valid",
+    "picture_warning":"Image size not valid",
+}
 
 def signup(request):
     if request.method=="POST":
@@ -12,6 +21,32 @@ def signup(request):
         gender=request.POST.get('gender')
         profile_picture=request.FILES.get('profile_picture')
         email=request.POST.get('email')
+
+        # username validate 
+        pattern = r'^[a-z0-9]+$'
+        if not re.match(pattern, username):
+            messages.warning(request,message_box['username_warning'])
+            return redirect('signup')
+
+        # password validate 
+        pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[a-zA-Z0-9@$!%*?&#]+$'
+        if not re.match(pattern, password):
+            messages.warning(request,message_box['password_warning'])
+            return redirect('signup')
+        
+        # city name validate 
+        pattern = r'^[a-zA-Z]+$'
+        if not re.match(pattern, city):
+            messages.warning(request,message_box['city_warning'])
+            return redirect('signup')
+        
+        # profile image validate 
+        max_size_bytes = 5 * 1024 * 1024
+        profile_picture.seek(0, 2)  # Move to the end of the file
+        file_size = profile_picture.tell()  # Get the current position, which is the file size
+        if file_size > max_size_bytes:
+            messages.warning(request, message_box['picture_warning'])
+            return redirect('signup')
         
         if password==confirm_password:
             user = CustomUserModel.objects.create_user(
